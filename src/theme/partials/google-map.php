2f8ec28c630abcea42d292b460e0d35afa633c00
Todd Milliken <tmilliken@emagine.com>
2018-03-10 18:25:52 -0500

    Added map directions.

--- src/theme/partials/google-map.php
+++ src/theme/partials/google-map.php
@@ -1,20 +1,20 @@
 <?php 
 
-$location = get_field( 'opts_map_location', 'options' ) ;
+$location = get_field( 'opts_map_location', 'options' );
+$content  = get_field( 'opts_map_content', 'options' );
 
 if ( ! empty( $location ) ) :
 ?>
 <section class="layer layer--google-map">
-	<div class="map-content">
-		<DIV class="inner">
-			<div class="map-content__inner">
-				<h2> Directions</h2>
-				<p>Lorem ipsum dolor amet 90's vaporware synth pinterest succulents art party butcher pour-over try-hard lumbersexual hell of literally taiyaki intelligentsia schlitz. </p>
-				<p>Tilde tattooed farm-to-table, kitsch literally wayfarers pickled organic poke crucifix viral live-edge four loko shabby chic pinterest. </p>
-				<a class="more" href="#">Contact Us</a>
+	<?php if ( ! empty( $content ) ) : ?>
+		<div class="map-content">
+			<DIV class="inner">
+				<div class="map-content__inner">
+					<?php echo $content; ?>
+				</div>
 			</div>
 		</div>
-	</div>
+	<?php endif; ?>
 	<div class="fullwidth-map acf-map">
 		<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>">
 			<?php if ( ! empty( get_field( 'opts_google_map_marker_content', 'options' ) ) ) : ?>
