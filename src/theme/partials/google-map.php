3a6314dee5b6e3f933654e63585d11c3a3c84d52
Todd Milliken <tmilliken@emagine.com>
2018-03-12 19:08:07 -0400

    Added fixes before launch for google maps responsive styles.

--- src/theme/partials/google-map.php
+++ src/theme/partials/google-map.php
@@ -1,4 +1,10 @@
-<?php 
+<?php
+/**
+ * Google Maps layer
+ *
+ * @since    1.0.0
+ * @package  Kraftwerke
+ */
 
 $location = get_field( 'opts_map_location', 'options' );
 $content  = get_field( 'opts_map_content', 'options' );
@@ -8,18 +14,16 @@ if ( ! empty( $location ) ) :
 <section class="layer layer--google-map">
 	<?php if ( ! empty( $content ) ) : ?>
 		<div class="map-content">
-			<DIV class="inner">
-				<div class="map-content__inner">
-					<?php echo $content; ?>
-				</div>
+			<div class="map-content__inner">
+				<?php echo wp_kses_post( $content ); ?>
 			</div>
 		</div>
 	<?php endif; ?>
 	<div class="fullwidth-map acf-map">
-		<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>">
+		<div class="marker" data-lat="<?php echo esc_attr( $location['lat'] ); ?>" data-lng="<?php echo esc_attr( $location['lng'] ); ?>">
 			<?php if ( ! empty( get_field( 'opts_google_map_marker_content', 'options' ) ) ) : ?>
 				<div class="marker-content">
-					<?php echo get_field( 'opts_google_map_marker_content', 'options' ); ?>
+					<?php the_field( 'opts_google_map_marker_content', 'options' ); ?>
 				</div>
 			<?php endif; ?>
 		</div>
