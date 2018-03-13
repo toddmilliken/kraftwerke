7efdc32b35fd46976c2f6de64690b5f69cd015df
Todd Milliken <tmilliken@emagine.com>
2018-03-13 09:14:29 -0400

    Added pagination to search results if needed.

--- src/theme/search.php
+++ src/theme/search.php
@@ -17,7 +17,11 @@ get_template_part('partials/masthead');
 /** Standard opening site wrapper */
 kwer_main_open();
 
+/** Displays the posts */
 get_template_part('partials/content-posts');
+
+/** Pagination */
+get_template_part('partials/pagination');
 
 /** Standard closing site wrapper */
 kwer_main_close();
